e5fafefb1f8a89252ade7a2fedf439231af2ad3e
Dmitry <kvasnikov.dmitry@gmail.com>
2026-09-17 12:17:14 +0000

    исправленный код

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@ void test_asan() {
     int* array = new int[5];
     array[0] = 10;
 
-    // выход за границы динамического массива
-    array[5] = 42; 
+    // выход за границы динамического массива. Исправлен
+    array[4] = 42; 
     
     std::cout << "ASan test value: " << array[0] << std::endl;
     delete[] array;
 }
 
 void test_ubsan() {
-    // деление на ноль
-    int num = 101/0;
+    // деление на ноль. Исправлен
+    int num = 101/1;
     
     std::cout << "UBSan test value: " << num << std::endl;
 }
